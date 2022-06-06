92561d86553d93a7da6312cf92884263d6c6eba1
kim mina <kimmina9070@gmail.com>
2022-06-07 00:32:34 +0900

    new version of ABS

--- Project_MIRIMRUM/Assets/Script/RendomAB.cs
+++ Project_MIRIMRUM/Assets/Script/RendomAB.cs
@@ -27,6 +27,35 @@ public class RendomAB : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    
+        area = GetComponent<BoxCollider>();
+
+        for (int i = 0; i < count; ++i)
+        {
+            Spawn();//생성 + 스폰위치를 포함하는 함수
+        }
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        Vector3 basePosition = transform.position;
+        Vector3 size = area.size;
+        float posX = basePosition.x + Random.Range(-size.x / 2f, size.x / 2f);
+        float posY = basePosition.y + Random.Range(-size.y / 2f, size.y / 2f);
+        float posZ = basePosition.z + Random.Range(-size.z / 2f, size.z / 2f);
+        Vector3 spawnPos = new Vector3(posX, posY, posZ);
+
+        return spawnPos;
+    }
+
+    private void Spawn()
+    {
+        int selection = Random.Range(0, prefabs.Length);
+
+        GameObject selectedPrefab = prefabs[selection];
+
+        Vector3 spawnPos = GetRandomPosition();//랜덤위치함수
+
+        GameObject instance = Instantiate(selectedPrefab, spawnPos, Quaternion.identity);
+        gameObject.Add(instance);
     }
 }
