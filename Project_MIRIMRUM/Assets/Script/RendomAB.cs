5c2a04524ca0b10b6633e468bbaf6a581d833ad1
kim mina <kimmina9070@gmail.com>
2022-06-06 23:43:14 +0900

    new update about ABS

--- Project_MIRIMRUM/Assets/Script/RendomAB.cs
+++ Project_MIRIMRUM/Assets/Script/RendomAB.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class RendomAB : MonoBehaviour
 {
-    // Start is called before the first frame update
+    blic GameObject[] prefabs; //찍어낼 게임 오브젝트를 넣어요
+                                 //배열로 만든 이유는 게임 오브젝트를
+                                 //다양하게 찍어내기 위해서 입니다
+    private BoxCollider area;    //박스콜라이더의 사이즈를 가져오기 위함
+    public int count = 3;      //찍어낼 게임 오브젝트 갯수
+
+    private List<GameObject> gameObject = new List<GameObject>();
+
     void Start()
     {
-        
+        area = GetComponent<BoxCollider>();
+
+        for (int i = 0; i < count; ++i)//count 수 만큼 생성한다
+        {
+            Spawn();//생성 + 스폰위치를 포함하는 함수
+        }
+
+        area.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        let z = Random.range(-5, 5);
-        let x = Random.range(-5, 5);
-        let y = 0.5;
-        GameObject enemy = (GameObject)Instantiate(obj, new Vector3(x, y, z),Quaternion.identity);
+    
     }
 }
